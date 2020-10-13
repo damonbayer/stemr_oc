943ea7a2784c4dea6815b696adc0e19d292e49af
Damon Bayer <damon@damonbayer.com>
2020-10-13 14:55:34 -0700

    Compare posteriors on estimation scale

--- check_posteriors.R
+++ check_posteriors.R
@@ -17,8 +17,9 @@ multi_chain_stem_fit <- read_rds("fixed_init_sim/multi_chain_stem_fit_R0D0.rds")
 stan_oc_post <- read_rds("fixed_init_sim/oc_post_more_precise.rds")
 stan_oc_model_objects <- read_rds("fixed_init_sim/model_objects.rds")
 
-human_scale_mcmc_list <- extract_stem_parameter_posterior(multi_chain_stem_fit, transform = to_human_scale)
 
+# Human Scale -------------------------------------------------------------
+human_scale_mcmc_list <- extract_stem_parameter_posterior(multi_chain_stem_fit, transform = to_human_scale)
 
 compare_posteriors <- stan_oc_post %>%
   spread_draws(alpha_incid_0,
@@ -41,16 +42,55 @@ compare_posteriors <- stan_oc_post %>%
          phi_death = sqrt_phi_inv_death,
          R0 = log_R0,
          rho_death = rho_death) %>%
-  mutate(R0 = exp(R0),
-         kappa = 1 / kappa^2,
-         phi_death = 1 / phi_death^2) %>%
+  mutate(R0 = exp(R0)) %>%
   mutate(model = "stan") %>%
   bind_rows(tidy_draws(human_scale_mcmc_list) %>%
               select(-starts_with(".")) %>%
               mutate(model = "stemr"))
 
+
+# Estimation Scale --------------------------------------------------------
+estim_scale_mcmc_list <- extract_stem_parameter_posterior(multi_chain_stem_fit)
+
+compare_posteriors <- stan_oc_post %>%
+  spread_draws(alpha_incid_0,
+               alpha_incid_1,
+               early_dur,
+               latent_dur,
+               prog_dur,
+               IFR,
+               sqrt_kappa_inv_incid,
+               sqrt_phi_inv_death,
+               log_R0,
+               rho_death) %>%
+  select(alpha0 = alpha_incid_0,
+         alpha1 = alpha_incid_1,
+         dur_early = early_dur,
+         dur_latent = latent_dur,
+         dur_progress = prog_dur,
+         ifr = IFR,
+         kappa = sqrt_kappa_inv_incid,
+         phi_death = sqrt_phi_inv_death,
+         R0 = log_R0,
+         rho_death = rho_death) %>%
+  mutate(alpha0 = log(alpha0),
+         alpha1 = logit(alpha1),
+         dur_early = -log(dur_early),
+         dur_latent = -log(dur_latent),
+         dur_progress = -log(dur_progress),
+         ifr = logit(ifr),
+         kappa = log(kappa),
+         phi_death = log(phi_death),
+         R0 = R0,
+         rho_death = logit(rho_death)) %>%
+  mutate(model = "stan") %>%
+  bind_rows(tidy_draws(estim_scale_mcmc_list) %>%
+              rename_all(~str_remove(., "_est")) %>%
+              select(-starts_with(".")) %>%
+              mutate(model = "stemr"))
+
+
 compare_posteriors %>%
-  mutate(phi_death = log(1 / sqrt(phi_death))) %>%
   pivot_longer(-model) %>%
   group_by(model, name) %>%
   median_qi() %>%
@@ -58,7 +98,6 @@ compare_posteriors %>%
   geom_pointinterval(position = position_dodge(width = .3)) +
   facet_wrap(. ~ name, scales = "free_x") +
   cowplot::theme_minimal_vgrid() +
-  # scale_x_log10() +
   xlab(NULL) +
   ggtitle("Fixed Inits")
 
